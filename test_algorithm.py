c75f332e87a0f7a50b7bc7cf488b0e159619d3f1
jcrosk <jcrosk@cox.net>
2023-10-08 12:19:13 -0500

    Added some tests from more complicated polynomials with known solutions.

--- test_algorithm.py
+++ test_algorithm.py
@@ -30,17 +30,65 @@ def test_4_leaf_star():
     assert independence_poly(star, coeffs=True) == [1, 4, 6, 5, 1]
 
 def test_custom_labels_star():
+    """
+    Tests that tree with max node as center is still properly calculated.
+    """
     # star with the max node as the center
     star = nx.Graph()
     star.add_edges_from([(5, 1), (5, 2), (5, 3)])
     assert independence_poly(star, coeffs=True) == [1, 3, 4, 1]
 
 def test_reconstructed_tree_isomorphic_to_original():
-    star = nx.star_graph(5)
-    print(star.edges)
-    _, mutated_tree = independence_poly(star, yield_tree=True)
-    print(mutated_tree.edges)
-    assert nx.isomorphism.tree_isomorphism(star, mutated_tree)
+    """
+    Tests that the graph is reconstructed correctly and is isomorphic to it's starting state.
+    """
+    original_star = nx.star_graph(5)
+    star = original_star.copy()
+    _ = independence_poly(star)
+    assert nx.isomorphism.tree_isomorphism(original_star, star)
 
+def test_claw_graph():
+    """
+    Tests graph T1 found on page 3 of: 
+    "On unimodality of independence polynomials of some well-covered trees" by Vadim Levit and Eugen Madrescu
+
+    https://www.academia.edu/818273/On_unimodality_of_independence_polynomials_of_some_well_covered_trees
+    """
+    graph = nx.Graph()
+    graph.add_edges_from([
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (3, 9),
+        (9, 10),
+        (3, 5), 
+        (5, 7), 
+        (5, 6),
+        (6, 8)
+    ])
+    assert independence_poly(graph, coeffs=True) == [14, 47, 60, 36, 10, 1]
+
+def test_claw_graph2():
+    """
+    Tests graph T2 found on page 3 of: 
+    "On unimodality of independence polynomials of some well-covered trees" by Vadim Levit and Eugen Madrescu
+
+    https://www.academia.edu/818273/On_unimodality_of_independence_polynomials_of_some_well_covered_trees
+    """
+    graph = nx.Graph()
+    graph.add_edges_from([
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (3, 9),
+        (9, 10),
+        (3, 5), 
+        (5, 7), 
+        (5, 6),
+        (6, 8),
+        (6, 11),
+        (11, 12),
+    ])
+    assert independence_poly(graph, coeffs=True) == [23, 93, 151, 125, 55, 12, 1]
     
     
